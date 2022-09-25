19994c10d5c27064e88bb1d18f117e3d38182f74
cznbnbnb <2364074108@qq.com>
2022-09-25 16:17:20 +0800

    JudgmentTest

---
+++ src/test/java/utils/JudgmentTest.java
@@ -0,0 +1,12 @@
+package utils;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class JudgmentTest {
+    @Test
+    public void JudgeTest(){
+        System.out.println(Judgment.Judge("D:/test/Answers.txt","D:/test/Ans.txt"));
+    }
+}
