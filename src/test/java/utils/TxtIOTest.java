12a67535dae89fee7dc13b158f495349de3cb4a2
cznbnbnb <2364074108@qq.com>
2022-09-25 12:00:31 +0800

    txtIOTest

--- src/test/java/utils/TxtIOTest.java
+++ src/test/java/utils/TxtIOTest.java
@@ -18,27 +18,27 @@ public class TxtIOTest {
         for (String string : strs) {
             System.out.println(string);
         }
+        System.out.println("读取成功...");
     }
 
     @Test
     public void writeTxtTest() {
         // 路径存在，正常写入
-        ArrayList<String> arrayList= TxtIOUtil.readTxt("D:/test/Answers.txt");;
+        ArrayList<String> arrayList= TxtIOUtil.readTxt("D:/test/Answers.txt");
         TxtIOUtil.writeTxt(arrayList, "D:/test/TxtIOTest.txt");
-
+        System.out.println("写入成功");
     }
 
     @Test
     public void readTxtFailTest() {
         // 路径不存在，读取失败
-        String str = TxtIOUtil.readTxt("D:/test/none.txt");
+        ArrayList<String> arrayList = TxtIOUtil.readTxt("D:/test/none.txt");
     }
 
     @Test
     public void writeTxtFailTest() {
         // 路径错误，写入失败
-        double[] elem = {0.11, 0.22, 0.33, 0.44, 0.55};
-        for (int i = 0; i < elem.length; i++) {
-            TxtIOUtil.writeTxt(elem[i], "User:/test/ans.txt");
+        ArrayList<String> arrayList= TxtIOUtil.readTxt("D:/test/Answers.txt");
+        TxtIOUtil.writeTxt(arrayList, "User:/test/none.txt");
         }
 }
